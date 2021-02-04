b5516fca3594c25a178a7b1c719d8c04286766cd
Nicholas <nicholasgfx@hotmail.com>
2021-02-03 20:13:59 -0600

    reworked program to re-render entire notes list upon adding a new note, hopefully allowing for an easier way to implement undo functionality

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ const notes = [{
     title: 'JavaScript',
     body: 'JavaScript defines how a web page behaves.',
 }];
-let noteCount = 0;
-let deletedNote
+let noteCounter = 0;
+let deletedNote;
 const submit = document.getElementById('submit');
 const noteList = document.getElementById('noteList');
 
+notes.forEach(note =>
+    //Renders each note on page load.
+
+    makeNoteCard(note.title, note.body)
+);
+
+submit.addEventListener('click', () => {
+    //Adds new notes to the notes array and html file
+
+    const newTitle = document.getElementById('newTitle').value;
+    const newBody = document.getElementById('newBody').value;
+    if (newTitle || newBody !== '') { //if note has any content, add it to the array, render it, and clear the input fields.
+        notes.push({
+            title: newTitle,
+            body: newBody
+        })
+        renderNotes();
+        document.getElementById('newTitle').value = '';
+        document.getElementById('newBody').value = '';
+    }
+})
+
+function renderNotes() {
+    //Clears all rendered notes, and re-renders them when a new note is added.
+
+    noteList.innerHTML = '';
+    noteCounter = 0;
+    notes.forEach(note => makeNoteCard(note.title, note.body));
+}
 
 function makeNoteCard(title, body) {
     //Makes new note cards and adds them to the rendered list.
-    
+
     const noteCard = document.createElement('div');
     noteCard.className = `noteCard`;
-    noteCard.id = `${noteCount}`
+    noteCard.id = `${noteCounter}`
     noteList.appendChild(noteCard);
-    noteCount++;
+    noteCounter++;
 
     const noteTitle = document.createElement('h3');
     noteTitle.className = 'noteTitle';
@@ -39,50 +68,10 @@ function makeNoteCard(title, body) {
     noteCard.appendChild(deleteButton);
 
     deleteButton.addEventListener('click', () => {
-        //notes.pop(deleteButton.parentElement.id)
+        //deletes the note, stores the previous content in the deletedNote variable for "undo" functionality.
+
         deletedNote = notes.splice(deleteButton.parentElement.id, 1);
-        noteCount--;
+        noteCounter--;
         deleteButton.parentElement.remove();
-        console.log(notes)
-        // notes.pop(noteCount)
-
-        // document.getElementById(`${noteCount}`).remove();
-        // noteCount--;
-        //add an index to the noteCard which can be referenced to delete it from the DOM and notes array
-
     })
 }
-
-notes.forEach(note =>
-    //Renders each note on page load.
-
-    makeNoteCard(note.title, note.body)
-);
-
-submit.addEventListener('click', () => {
-    //Adds new notes to the notes array and html file
-
-    const newTitle = document.getElementById('newTitle').value;
-    const newBody = document.getElementById('newBody').value;
-    if (newTitle || newBody !== '') {
-        makeNoteCard(newTitle, newBody);
-        document.getElementById('newTitle').value = '';
-        document.getElementById('newBody').value = '';
-        notes.push({
-            title: newTitle,
-            body: newBody
-        })
-    }
-})
-
-
-
-/*
-Basic Remove Note Logic
-notes.find('note name') //return the index and splice it out while
-store it in a variable to enable the user to undo the action.
-
-
-FUNCTIONALITY
-MODIFY NOTES ADD LATER. Same logic as new note mostly.
-*/
